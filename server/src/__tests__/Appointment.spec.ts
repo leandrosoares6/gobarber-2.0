a864adcac1a9489b69a7077c933493473f5a7e67
Leandro Neves <leandrosoares6@hotmail.com>
2020-04-29 12:16:18 -0300

    New test for appointments in the same time added

--- server/src/__tests__/Appointment.spec.ts
+++ server/src/__tests__/Appointment.spec.ts
@@ -1,10 +1,14 @@
 import request from 'supertest';
 import { isUuid } from 'uuidv4';
+import { startOfHour, isEqual } from 'date-fns';
 import app from '../app';
+import appointmentsRouter from '../routes/appointments.routes';
 
 describe('Appointment', () => {
   it('should be able to create a new appointment', async () => {
     const date = new Date();
+    const parsedDate = startOfHour(date);
+
     const response = await request(app).post('/appointments').send({
       provider: 'Leandro',
       date: date.toISOString(),
@@ -14,7 +18,19 @@ describe('Appointment', () => {
 
     expect(response.body).toMatchObject({
       provider: 'Leandro',
-      date: date.toISOString(),
+      date: parsedDate.toISOString(),
     });
+  });
+
+  it('should not permitted create appointments at the same time', async () => {
+    const date = new Date();
+    const parsedDate = startOfHour(date);
+
+    const response = await request(app).post('/appointments').send({
+      provider: 'Leandro',
+      date: parsedDate.toISOString(),
+    });
+
+    expect(response.status).toBe(400);
   });
 });
