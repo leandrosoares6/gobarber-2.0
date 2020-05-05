5b1cea0dbe0a01934f0b5081ac1ceef5c9527ca6
Leandro Neves <leandrosoares6@hotmail.com>
2020-05-05 16:55:24 -0300

    Serving static files

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express from 'express';
+import uploadConfig from './config/upload';
 import routes from './routes';
 import createTypeormConnections from './database';
 
@@ -7,6 +8,7 @@ const app = express();
 
 createTypeormConnections().then(() => {
   app.use(express.json());
+  app.use('/files', express.static(uploadConfig.directory));
   app.use(routes);
 });
 
