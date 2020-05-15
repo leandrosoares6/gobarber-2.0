b9cd660eb05ea6664ebbb1d631bef4f2da6988c2
Leandro Neves <leandrosoares6@hotmail.com>
2020-05-15 13:50:32 -0300

    ListProviderMonthAvailabilityService and test initialised

--- server/src/shared/infra/http/routes/index.ts
+++ server/src/shared/infra/http/routes/index.ts
@@ -3,15 +3,19 @@ import { Router } from 'express';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
-import appointmentRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+
+import appointmentRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import providersRouter from '@modules/appointments/infra/http/routes/providers.routes';
 
 const routes = Router();
 
-routes.use('/appointments', appointmentRouter);
+routes.use('/sessions', sessionsRouter);
 routes.use('/users', usersRouter);
 routes.use('/profile', profileRouter);
-routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+
+routes.use('/appointments', appointmentRouter);
+routes.use('/providers', providersRouter);
 
 export default routes;
