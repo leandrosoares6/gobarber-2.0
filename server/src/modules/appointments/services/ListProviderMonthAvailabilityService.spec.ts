d37e1f51bfa03b69d62b39edf345f9e54bb15dc0
Leandro Neves <leandrosoares6@hotmail.com>
2020-05-15 22:23:52 -0300

    ListProviderMonthAvailabilityService and test completed

--- server/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ server/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -13,25 +13,18 @@ describe('ListProviderMonthAvailability', () => {
   });
 
   it('should be able to list days of month availability from provider', async () => {
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      date: new Date(2020, 3, 20, 8, 0, 0),
-    });
+    const schedule = Array.from({ length: 10 }, (_, i) => i + 1).map(
+      value => value + 7,
+    );
 
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      date: new Date(2020, 4, 20, 8, 0, 0),
-    });
-
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      date: new Date(2020, 4, 20, 10, 0, 0),
-    });
-
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      date: new Date(2020, 4, 21, 8, 0, 0),
-    });
+    Promise.all(
+      schedule.map(hour =>
+        fakeAppointmentsRepository.create({
+          provider_id: 'user',
+          date: new Date(2020, 4, 20, hour, 0, 0),
+        }),
+      ),
+    );
 
     const availability = await listProviderMonthAvailability.execute({
       provider_id: 'user',
@@ -43,7 +36,7 @@ describe('ListProviderMonthAvailability', () => {
       expect.arrayContaining([
         { day: 19, available: true },
         { day: 20, available: false },
-        { day: 21, available: false },
+        { day: 21, available: true },
         { day: 22, available: true },
       ]),
     );
