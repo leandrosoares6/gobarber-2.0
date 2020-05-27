473a1f515aead2c20a1052c52c62874daf290dfa
Leandro Neves <leandrosoares6@hotmail.com>
2020-05-27 15:53:10 -0300

    Frontend web app template initialised

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const src: React.FC = () => {
+  return (
+    <div>
+      <p>Hello World!</p>
+    </div>
+  );
+};
+
+export default src;
